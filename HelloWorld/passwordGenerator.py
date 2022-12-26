7963d19da51369ec1dfd996cff4ffaac63864006
jashkarangiya <jashkarangiya@gmail.com>
2022-12-26 11:15:32 +0530

    passwordGenerator.py added

---
+++ HelloWorld/passwordGenerator.py
@@ -0,0 +1,16 @@
+import random
+
+list_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+list_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+list_symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '`~']
+
+print("Welcome to the PyPassword Generator!\n")
+letters = int(input("How many letters would you like in your password? "))
+symbols = int(input("How many symbols would you like? "))
+numbers = int(input("How many number would you like? "))
+
+for letter in list_letters:
+
+
+
+
