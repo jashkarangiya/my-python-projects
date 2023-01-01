a3052fa076d4a522bec52939f214f09aeb662c20
jashkarangiya <jashkarangiya@gmail.com>
2023-01-01 22:41:17 +0530

    Updated simpleInterest.py

--- HelloWorld/College Practicals/simpleInterest.py
+++ HelloWorld/College Practicals/simpleInterest.py
@@ -1,9 +1,11 @@
 print("This is a program to calculate simple interest")
 
+#Taking inputs from the users
 principleAmount = int(input("\nEnter a amount: ₹ "))
 rate = int(input("Enter rate of interest: "))
 time = int(input("Enter number of years: "))
 
+#Formula to find simple interest
 simpleI = (principleAmount * rate * time ) / 100
 
 print(f"\nSimple Interest: {simpleI}")
