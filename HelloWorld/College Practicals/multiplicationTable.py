06356d5ea1d55b21c38e2a3d9bf5bed91d09ba38
jashkarangiya <jashkarangiya@gmail.com>
2023-01-13 21:58:26 +0530

    Updated mutliplicationTable.py - 13/01/2023

--- HelloWorld/College Practicals/multiplicationTable.py
+++ HelloWorld/College Practicals/multiplicationTable.py
@@ -1,6 +1,6 @@
+def multiplicationTable(num):
+   for i in range(1, 11):
+      print(num, 'x', i, '=', num * i)
 
-# To take input from the user
 num = int(input("Display multiplication table of? "))
-
-for i in range(1, 11):
-   print(num, 'x', i, '=', num*i)
+multiplicationTable(num)
