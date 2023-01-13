1d610d7fa78d0bd175f1d5a401b30e4b9cc664d8
jashkarangiya <jashkarangiya@gmail.com>
2023-01-13 18:22:58 +0530

    Added some new files - 13/01/2023

---
+++ HelloWorld/Challenges/hurdle4.py
@@ -0,0 +1,30 @@
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+#
+# while at_goal() != True:
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+print("This code would run only on: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json")
