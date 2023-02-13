08153654997f038511883da059c2870d3f5cba93
jashkarangiya <jashkarangiya@gmail.com>
2023-02-13 13:10:12 +0530

    Updated hangman.py file - 13/02/2023

--- HelloWorld/Challenges/Udemy Course Challenge/Challenge/hangman.py
+++ HelloWorld/Challenges/Udemy Course Challenge/Challenge/hangman.py
@@ -3,40 +3,21 @@ import random
 word_list = ["camel", "hello", "apple"]
 
 wordSelected = random.choice(word_list).lower()
-# print(wordSelected)
+print(wordSelected)
 
 
 # print(letterGuessed)
 list = []
 for letters in wordSelected:
-    i = 0
     list.append("_")
+
+i = 0
+for letters in wordSelected:
     letterGuessed = input("Enter a letter: ").lower()
 
     if letters == letterGuessed:
         list[i] = letters
+        print(list)
     else:
         print("noob")
     i += 1
-
-print(list)
-
-
-
-# for letters in wordSelected:
-#     if letters == letterGuessed:
-#         print("True")
-#     else:
-#         print("False")
-
-
-
-
-
-
-
-
-
-
-
-
