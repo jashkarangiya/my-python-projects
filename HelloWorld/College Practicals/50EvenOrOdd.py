fdc9eb1313b39c95226778480d0a6a32e6e0588d
jashkarangiya <jashkarangiya@gmail.com>
2023-02-20 12:12:59 +0530

    Added 50EvenOrOdd.py file - 13/02/2023

---
+++ HelloWorld/College Practicals/50EvenOrOdd.py
@@ -0,0 +1,11 @@
+even_numbers = [x for x in range(1, 51) if x % 2 == 0]
+odd_numbers = [x for x in range(1, 51) if x % 2 != 0]
+print("list of even numbers upto 50\n")
+print(even_numbers)
+print("\nlist of odd numbers upto 50\n")
+print(odd_numbers)
+divided_by_5 = [x for x in range(1, 101) if x % 5 == 0]
+print("list of numbers that are divided by 5 upto 100")
+print(divided_by_5)
+
+print("\nAuthor: Jash Karangiya\nID NO. 21DCE042")
