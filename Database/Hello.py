2ef20a4d7024d37c8d953e9744365d7309baa106
jashkarangiya <jashkarangiya@gmail.com>
2023-03-13 12:56:01 +0530

    Adding databse in python - College Practical

---
+++ Database/Hello.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+conn= mysql.connector.connect(
+    host= "localhost",
+    user = "21DCE042 Jash",
+    password= "Jash@1265##",
+    database = "Levi"
+)
+
+print(conn)
+
+mycursor = conn.cursor()
+# mycursor.execute("CREATE DATABASE Levi")
+# mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE TABLE employee(emp_id int, emp_name varchar(20), salary decimal(15,2))")
+
+# sql = "INSERT INTO employee values(69, 'Levi Ackerman', 42000)"
+
+
+# Update query:
+# sql = "UPDATE employee SET emp_name = 'Levi' WHERE emp_id = 69"
+
+# Delete query:
+# sql = "DELETE FROM employee where emp_id=69"
+
+mycursor.execute(sql)
+
+conn.commit()
