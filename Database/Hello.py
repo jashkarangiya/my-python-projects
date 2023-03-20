812364aa3a097b054e95422c97ea8f3461b2071c
jashkarangiya <jashkarangiya@gmail.com>
2023-03-20 09:27:51 +0530

    Formatted the code

--- Database/Hello.py
+++ Database/Hello.py
@@ -4,28 +4,54 @@ conn= mysql.connector.connect(
     host= "localhost",
     user = "21DCE042 Jash",
     password= "Jash@1265##",
-    database = "Levi"
+    database = "LeviAckerman"
 )
 
 print(conn)
 
 mycursor = conn.cursor()
-# mycursor.execute("CREATE DATABASE Levi")
-# mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE TABLE employee(emp_id int, emp_name varchar(20), salary decimal(15,2))")
+# Database Created
+mycursor.execute("CREATE DATABASE LeviAckerman")
 
-# sql = "INSERT INTO employee values(69, 'Levi Ackerman', 42000)"
+# Table created
+mycursor.execute("CREATE TABLE employee(emp_id int, emp_name varchar(20), salary decimal(15,2))")
+
+#Insert Query
+mycursor.execute("INSERT INTO employee values(69, 'Levi Ackerman', 42000)")
+mycursor.execute("INSERT INTO eDmployee values(70, 'Mikasa Ackerman', 69000)")
+
+records = mycursor.fetchall()
+
+# For printing data from db
+for record in records:
+    print(record)
 
 
 # Update query:
-sql = "UPDATE employee SET emp_name = 'Levi' WHERE emp_id = 69"
+mycursor.execute("UPDATE employee SET emp_name = 'Jash Karangiya' WHERE emp_id = 69")
+
+records = mycursor.fetchall()
+
+# For printing data from db
+for record in records:
+    print(record)
+
+# Select Operations
+mycursor.execute("SELECT * FROM employee")
+records = mycursor.fetchall()
+
+# For printing data from db
+for record in records:
+    print(record)
 
 # Delete query:
-# sql = "DELETE FROM employee where emp_id=69"
+mycursor.execute("DELETE FROM employee where emp_id=69")
 
+records = mycursor.fetchall()
 
-
-mycursor.execute(sql)
+# For printing data from db
+for record in records:
+    print(record)
 
 conn.commit()
