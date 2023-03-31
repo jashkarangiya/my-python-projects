6c68bdd83bd860c9a907b48ea29252f37a8bcf18
jashkarangiya <jashkarangiya@gmail.com>
2023-03-31 10:06:26 +0530

    Added 8th Set

--- HelloWorld/College Practicals/Assignment 2/Set 8/1.py
+++ HelloWorld/College Practicals/Assignment 2/Set 8/1.py
@@ -2,3 +2,20 @@
 # Containing multiple words. Print back to the user the same string, except with the words in
 # backwards order. For example, say I type the string: My name is Michele Then I would see
 # the string: Michele is name My shown back to me.
+
+def printingBackwardString(string):
+    list1 = string.split()
+    # print(list1)
+
+    list1 = list(reversed(list1))
+    # print(list1)
+
+    string= ' '.join(list1)
+    # print(string)
+    return string
+
+
+string = input("Enter a string: ")
+result = printingBackwardString(string)
+print(f"\nString : {string}. \nReversed String: {result}")
+print(f"\nThe string is '{string}' is now converted to '{result}'.")
