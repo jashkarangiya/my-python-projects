031bbb740ca97aa8f2aee8f442fea64b36e1b8c1
jashkarangiya <jashkarangiya@gmail.com>
2023-03-31 10:07:31 +0530

    Added 12th Set

--- HelloWorld/College Practicals/Assignment 2/Set 12/1.py
+++ HelloWorld/College Practicals/Assignment 2/Set 12/1.py
@@ -1,3 +1,23 @@
 # 12.1 Create two 2D Numpy arrays with random numbers and concatenate them using the
 # Numpy library. After Concatenation, reshape the resulting Numpy array such that the number
 # of rows and columns is reversed.
+
+import numpy as np
+
+# create two 2D numpy arrays with random numbers
+arr1 = np.random.rand(2, 3)
+arr2 = np.random.rand(2, 4)
+
+print("Array 1:\n", arr1)
+print("Array 2:\n", arr2)
+
+# concatenate the two arrays
+concatenated = np.concatenate((arr1, arr2), axis=1)
+
+print("Concatenated array:\n", concatenated)
+
+# reshape the concatenated array
+reshaped = concatenated.reshape(concatenated.shape[1], concatenated.shape[0])
+
+print("Reshaped array:\n", reshaped)
+print("\nID: 21DCE042\nAuthor: Jash Karangiya")
--- HelloWorld/College Practicals/Assignment 2/Set 12/2.py
+++ HelloWorld/College Practicals/Assignment 2/Set 12/2.py
@@ -1,2 +1,36 @@
 # 12.2 Create a Pandas series from a Python List. Find out the mean, median, mode, range and
 # standard deviation of the series.
+
+import pandas as pd
+import numpy as np
+
+# create a list of random numbers
+num_list = [np.random.randint(1, 10) for i in range(10)]
+
+# create a Pandas series from the list
+num_series = pd.Series(num_list)
+
+# find the mean
+mean = num_series.mean()
+
+# find the median
+median = num_series.median()
+
+# find the mode
+mode = num_series.mode().values
+
+# find the range
+range_val = num_series.max() - num_series.min()
+
+# find the standard deviation
+std_dev = num_series.std()
+
+# print the statistical measures
+print("Original Series:\n", num_series)
+print("Mean:", mean)
+print("Median:", median)
+print("Mode:", mode)
+print("Range:", range_val)
+print("Standard Deviation:", std_dev)
+
+print("\nID: 21DCE042\nAuthor: Jash Karangiya")
