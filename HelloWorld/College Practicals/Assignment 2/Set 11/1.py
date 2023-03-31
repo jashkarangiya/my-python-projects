e3bb5a7041fa7153126ed7b0b2af8ef789c63ddb
jashkarangiya <jashkarangiya@gmail.com>
2023-03-31 10:07:22 +0530

    Added 11th Set

--- HelloWorld/College Practicals/Assignment 2/Set 11/1.py
+++ HelloWorld/College Practicals/Assignment 2/Set 11/1.py
@@ -1,2 +1,40 @@
 # Develop programs to understand the working of exception handling with the user guessing a
 # number until he/she gets it right.
+
+import random
+
+
+def guess_number():
+    # generate a random number between 1 and 100
+    secret_number = random.randint(1, 100)
+
+    # initialize guess to an invalid value to enter the loop
+    guess = 0
+
+    # loop until the guess is correct
+    while guess != secret_number:
+        try:
+            # ask user to guess the number
+            guess = int(input("Guess a number between 1 and 100: "))
+
+            # check if guess is out of range
+            if guess < 1 or guess > 100:
+                raise ValueError("Guess must be between 1 and 100")
+
+            # check if guess is correct
+            if guess == secret_number:
+                print("Congratulations! You guessed the number!")
+            elif guess < secret_number:
+                print("aim higher")
+            else:
+                print("aim lower")
+
+
+        except ValueError as e:
+            # handle invalid input from user
+            print("Invalid input:", e)
+
+
+if __name__ == '__main__':
+    guess_number()
+print("\nID: 21DCE042\nAuthor: Jash Karangiya")
